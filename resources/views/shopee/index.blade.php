c94c6313bafbfedb88cea3a50ef86d7fec7cfb48
nashirjamali <nashirjamali@gmail.com>
2019-11-14 20:11:49 +0700

    tokopedia shopee

--- resources/views/shopee/index.blade.php
+++ resources/views/shopee/index.blade.php
@@ -2,66 +2,62 @@
 
 @section('content')
 
-    <div class="container mt-3">
-        <h1 class="text-center">Good Price</h1>
-        <div class="row mt-2">
-            <div class="col-2">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-                    <label class="form-check-label" for="inlineCheckbox1">Nama</label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option2">
-                    <label class="form-check-label" for="inlineCheckbox1">Harga</label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option3">
-                    <label class="form-check-label" for="inlineCheckbox1">Deskripsi</label>
-                </div>
-            </div>
-            <div class="col-8">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option4">
-                    <label class="form-check-label" for="inlineCheckbox1">Review</label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option5">
-                    <label class="form-check-label" for="inlineCheckbox1">Produk Terkirim</label>
-                </div>
-            </div>
-        </div>
-        
-        <button class="btn btn-primary mt-2" id="btn-find">Ambil Data</button>
+<div class="container mt-3">
+    <h1 class="text-center">Good Price</h1>
 
-        <div class="table-responsive mt-3">
-          <table class="table" id="table">
-                 <thead>
-                  <tr>
-                      <th width="10%">Nama</th>
-                      <th width="35%">Harga</th>
-                      <th width="35%">Deskripsi</th>
-                      <th width="30%">Review</th>
-                      <th width="30%">Produk Terkirim</th>
-                  </tr>
-                 </thead>
-             </table>
-         </div>
-        <div class="text-center">
-            <button type="button" class="btn btn-primary">Tokenizing</button>
-            <button type="button" class="btn btn-secondary">Case Folding</button>
-            <button type="button" class="btn btn-success">Stemming</button>
-            <button type="button" class="btn btn-info">Stopword</button>
-            <div class="mt-3">
-                <button type="button" class="btn btn-info">Hapus Kata Tanya</button>
-                <button type="button" class="btn btn-success">Hapus Tanda Baca</button>
-                <button type="button" class="btn btn-warning">Hapus Emoticon</button>
+
+    <div class="row mt-2">
+        <div class="col-2">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="atribute[]" id="name" value="name">
+                <label class="form-check-label" for="namecb">Nama</label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="atribute[]" id="price" value="price">
+                <label class="form-check-label" for="pricecb">Harga</label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="atribute[]" id="description" value="description">
+                <label class="form-check-label" for="descriptioncb">Deskripsi</label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="atribute[]" id="rating" value="description">
+                <label class="form-check-label" for="descriptioncb">Rating</label>
             </div>
         </div>
     </div>
+    <div class="text-center my-5">
+        <button id="tokenizing" class="btn btn-primary">Tokenizing</button>
+        <button id="case-folding" class="btn btn-secondary">Case Folding</button>
+        <button id="stemming" class="btn btn-success">Stemming</button>
+        <button id="stopword" class="btn btn-info">Stopword</button>
+        <div class="mt-3">
+            <button id="katatanya" class="btn btn-info">Hapus Kata Tanya</button>
+            <button id="tandabaca" class="btn btn-success">Hapus Tanda Baca</button>
+            <button id="removemot" class="btn btn-warning">Hapus Emoticon</button>
+        </div>
+    </div>
+
+
+    <div class="table-responsive mt-3">
+        <table class="table" id="table-shopee" width="100%">
+            <thead>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </thead>
+        </table>
+    </div>
+</div>
 
 @endsection
 
 
 @push('footer-scripts')
+<script src="{{ asset('js/shopee.js') }}">
+</script>
 @endpush
-
--- routes/web.php
+++ routes/web.php
@@ -15,28 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/tokopedia', 'TokopediaSourceController@index')->name('tokopedia.index');
+Route::get('/tokopedia/search/{method}','TokopediaSourceController@search')->name('tokopedia.search');
 
 Route::get('/shopee', 'ShopeeSourceController@index')->name('shopee.index');
-
-
-Route::get('/shopee/stemming','ShopeeSourceController@stemming')->name('shopee.stemming');
-Route::get('/shopee/token','ShopeeSourceController@tokenazing')->name('shopee.token');
-Route::get('/shopee/case','ShopeeSourceController@casefolding')->name('shopee.case');
-Route::get('/shopee/stopword','ShopeeSourceController@stopword')->name('shopee.stopword');
-Route::get('/shopee/tandabaca','ShopeeSourceController@tandabaca')->name('shopee.tandabaca');
-Route::get('/shopee/katatanya','ShopeeSourceController@katatanya')->name('shopee.katatanya');
-Route::get('/shopee/removemot','ShopeeSourceController@removemot')->name('shopee.removemot');
-
-Route::get('/tokopedia', 'TokopediaSourceController@index')->name('tokopedia.index');
-
-
-Route::get('/tokopedia/stemming','TokopediaSourceController@stemming')->name('tokopedia.stemming');
-Route::get('/tokopedia/token','TokopediaSourceController@tokenazing')->name('tokopedia.token');
-Route::get('/tokopedia/case','TokopediaSourceController@casefolding')->name('tokopedia.case');
-Route::get('/tokopedia/stopword','TokopediaSourceController@stopword')->name('tokopedia.stopword');
-Route::get('/tokopedia/tandabaca','TokopediaSourceController@tandabaca')->name('tokopedia.tandabaca');
-Route::get('/tokopedia/katatanya','TokopediaSourceController@katatanya')->name('tokopedia.katatanya');
-Route::get('/tokopedia/removemot','TokopediaSourceController@removemot')->name('tokopedia.removemot');
-
+Route::get('/shopee/search/{method}','ShopeeSourceController@search')->name('shopee.search');
 
 Route::get('/mediated-schema', 'MediatedSchemaController@index')->name('mediated-schema.index');
