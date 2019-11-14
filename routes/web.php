4e1b53d35ff92d9c0e6ca3180e5997cf8c1b46a4
ibnuprtma <pibnu86@gmail.com>
2019-11-14 12:03:22 +0700

    function token etc

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,24 @@ Route::get('/', function () {
 Route::get('/shopee', 'ShopeeSourceController@index')->name('shopee.index');
 
 
+Route::get('/shopee/stemming','ShopeeSourceController@stemming')->name('shopee.stemming');
+Route::get('/shopee/token','ShopeeSourceController@tokenazing')->name('shopee.token');
+Route::get('/shopee/case','ShopeeSourceController@casefolding')->name('shopee.case');
+Route::get('/shopee/stopword','ShopeeSourceController@stopword')->name('shopee.stopword');
+Route::get('/shopee/tandabaca','ShopeeSourceController@tandabaca')->name('shopee.tandabaca');
+Route::get('/shopee/katatanya','ShopeeSourceController@katatanya')->name('shopee.katatanya');
+Route::get('/shopee/removemot','ShopeeSourceController@removemot')->name('shopee.removemot');
+
 Route::get('/tokopedia', 'TokopediaSourceController@index')->name('tokopedia.index');
+
+
+Route::get('/tokopedia/stemming','TokopediaSourceController@stemming')->name('tokopedia.stemming');
+Route::get('/tokopedia/token','TokopediaSourceController@tokenazing')->name('tokopedia.token');
+Route::get('/tokopedia/case','TokopediaSourceController@casefolding')->name('tokopedia.case');
+Route::get('/tokopedia/stopword','TokopediaSourceController@stopword')->name('tokopedia.stopword');
+Route::get('/tokopedia/tandabaca','TokopediaSourceController@tandabaca')->name('tokopedia.tandabaca');
+Route::get('/tokopedia/katatanya','TokopediaSourceController@katatanya')->name('tokopedia.katatanya');
+Route::get('/tokopedia/removemot','TokopediaSourceController@removemot')->name('tokopedia.removemot');
 
 
 Route::get('/mediated-schema', 'MediatedSchemaController@index')->name('mediated-schema.index');
